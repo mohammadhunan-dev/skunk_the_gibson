dc77f5cc04501fccfcb53ad7bdb4fd8fa9b0b961
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-07-17 11:52:27 -0400

    updated ft w/ changes

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, request, send_from_directory, render_template
 from flask_pymongo import PyMongo
+import time
 
-app = Flask(__name__,  static_url_path='/views')
+app = Flask(__name__,  static_url_path='/static')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config["MONGO_URI"] = "mongodb://localhost:27017/skunkWorks_db"
 mongo = PyMongo(app)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/app/scene2')
+def scenetwo():
+    return render_template('scene2.html')
 
 @app.route('/app/')
 def hello():
@@ -36,27 +41,32 @@ def hello():
                 'color': "#33ee22"
             }]
 
-    return render_template('index.html', boxes=boxes)
+    ts = time.time()
+    return render_template('index.html', boxes=boxes, last_updated = ts)
 
 
 @app.route('/collections/', methods=['POST'])
 def collection_data():
-    data = request.data
-    dataDict = json.loads(data)
-    db = dataDict['db']
-    username = dataDict['user']
-    password = dataDict['password']
+    # data = request.data
+    # dataDict = json.loads(data)
+    # db = dataDict['db']
+    # username = dataDict['user']
+    # password = dataDict['password']
 
-    return 'TODO: fetch collections and get document counts for each'
+    # return 'TODO: fetch collections and get document counts for each'
+    return { "foo": "bar"}
+
 
 @app.route('/documents/', methods=['POST'])
 def document_data():
-    data = request.data
-    dataDict = json.loads(data)
-    db = dataDict['db']
-    username = dataDict['user']
-    password = dataDict['password']
-    collection = dataDict['collection']
-    collectionFilter = dataDict['collectionFilter']
+    # data = request.data
+    # dataDict = json.loads(data)
+    # db = dataDict['db']
+    # username = dataDict['user']
+    # password = dataDict['password']
+    # collection = dataDict['collection']
+    # collectionFilter = dataDict['collectionFilter']
 
-    return 'TODO'
+    return { "foo" : "bar2" }
+
+
--- static/index.js
+++ static/index.js
@@ -1,1 +1,48 @@
-console.log('hello from static/index.js' )
+console.log('hello from static/index.js!!' );
+
+AFRAME.registerComponent('initial-scene-load-event', {
+    schema: {
+      color: {default: 'orange'}
+    },
+
+    init: function () {
+   
+        fetch("/collections/", { method: "POST", credentials: "same-origin"}).then((response) => {
+            response.json().then((data) => {
+                console.log(data)
+            })
+        })
+    }
+  });
+
+
+AFRAME.registerComponent('load-document-event', {
+    schema: {
+      color: {default: 'orange'}
+    },
+
+    init: function () {
+      var data = this.data;
+      var el = this.el;  // <a-box>
+      var defaultColor = el.getAttribute('material').color;
+
+      el.addEventListener('click', function (ev) {
+        // mouse enter and or click
+        console.log('doc',document.querySelector('a-entity'))
+        document.querySelector('a-entity').setAttribute('environment',"preset: goldmine; skyType: atmosphere")
+        fetch("/documents/", { method: "POST", credentials: "same-origin"}).then((response) => {
+            response.json().then((data) => {
+                console.log(data)
+            })
+        })
+        el.setAttribute('color', "orange");
+      });
+
+      el.addEventListener('mouseleave', function () {
+        // mouse exit
+        el.setAttribute('color', defaultColor);
+      });
+  
+    }
+  });
+
